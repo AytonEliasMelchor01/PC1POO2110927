7b06a8db2d993df9b17ddc73cd18f0c80452fc4d
AytonEliasMelchor01 <86389838+AytonEliasMelchor01@users.noreply.github.com>
2023-09-17 13:40:41 -0500

    Add files via upload

---
+++ 10.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class E9
+{
+	public static void main(String[] args) {
+	    final double gravedad = 9.81; //definir la gravedad como una constante 
+	    Scanner input = new Scanner (System.in);
+
+	    System.out.println("Cálculo del tiempo que tarda en caer un objeto desde cierta altura ");
+	    System.out.println("Ingrese la altura en metros: ");
+	    double Altura = input.nextDouble(); //pedir ingresar altura en metros
+	    
+	    double Tiempo = Math.sqrt((2*Altura)/gravedad); //calcular usando la fórmula t = sqrt(2h/g)
+		System.out.println("El tiempo que tarda en caer es: "+ Tiempo +" segundos");
+		
+	}
+}
