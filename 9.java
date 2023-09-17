349a46ccc4fd2b25e1f24b0d7e0fae7322ec065a
AytonEliasMelchor01 <86389838+AytonEliasMelchor01@users.noreply.github.com>
2023-09-17 13:00:01 -0500

    Add files via upload

---
+++ 9.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class E9 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner (System.in);
+    int sueldoSemanal;
+    int horasTrabajadas;
+
+    System.out.print("Por favor, introduzca el número de horas trabajadas durante la semana: ");
+    horasTrabajadas = input.nextInt();
+    
+    if (horasTrabajadas <= 40) {
+      sueldoSemanal = 12 * horasTrabajadas;
+    } else {
+      sueldoSemanal = (40 * 12) + ((horasTrabajadas - 40) * 16);
+    }
+    System.out.println("El sueldo semanal que le corresponde es de " + sueldoSemanal + " euros");
+  }
+}
