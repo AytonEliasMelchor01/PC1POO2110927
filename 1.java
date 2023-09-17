e9ebe7c54f6ce0dcc1b4c491ed532cd79168b41c
AytonEliasMelchor01 <86389838+AytonEliasMelchor01@users.noreply.github.com>
2023-09-17 02:07:54 -0500

    Add files via upload

---
+++ 1.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class E1
+{
+	public static void main(String[] args) {
+	    int x=144;
+	    int y=999;
+	    int suma = x+y ;
+	    int resta = x-y;
+	    int multi = x*y;
+	    float division = x/y;
+	    
+	    
+		System.out.println("valor de x = "+ x);
+		System.out.println("valor de y = "+ y);
+		System.out.println("suma = "+ suma);
+		System.out.println("resta = " + resta);
+		System.out.println("multiplicación = "+multi);
+		System.out.println("división = "+ division);
+	}
+}
