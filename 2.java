c003178aebabeb990dfa0e4554178c0db3354180
AytonEliasMelchor01 <86389838+AytonEliasMelchor01@users.noreply.github.com>
2023-09-17 02:30:58 -0500

    Add files via upload

---
+++ 2.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class E2
+{
+	public static void main(String[] args) {
+	    String nombre = "Ayton Elias Melchor";
+	    String dirección = "av.Las palmas villa riviera Pachacamac - Lima";
+	    int teléfono = 922296050 ;
+		System.out.println("nombre: "+ nombre);
+		System.out.println("dirección: "+ dirección);
+		System.out.println("teléfono: "+ teléfono);
+
+	}
+}
